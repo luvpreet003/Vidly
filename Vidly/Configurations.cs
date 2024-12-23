624502bcc9578fcb946d79a988fb8132f70d3cac
Luvpreet Singh <luvpreetsunny@gmail.com>
2024-12-23 15:23:01 +0530

    added migrations config

---
+++ Vidly/Configurations.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using Vidly.Models;
+
+namespace Vidly
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection") // Connection string name in Web.config or appsettings.json
+        {
+
+        }
+
+        // Add DbSet properties for your entities
+        public DbSet<Movies> Movies { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
