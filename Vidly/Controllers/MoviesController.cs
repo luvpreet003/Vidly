163eb50e088746683d8f2d9b54d8a406716baabf
Luvpreet Singh <luvpreetsunny@gmail.com>
2024-12-23 14:16:19 +0530

    Added basic details and movies and customers tab

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        public ViewResult MovieIndex()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movies> GetMovies()
+        {
+            return new List<Movies>
+            {
+                new Movies { Id = 1, Title = "Shrek" },
+                new Movies { Id = 2, Title = "Wall-e" }
+            };
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movies()
+            {
+                Id = 1,
+                Title = "Jawan"
+            };
+
+            var customer = new List<Customer>()
+            {
+                new Customer{Name = "Rahul"},
+                new Customer{Name = "Dravid"},
+                new Customer{Name = "wall"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customer
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
